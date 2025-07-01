e6299416c280055908b88ea68a52e2197cd40174
NoSayMe <45827354+NoSayMe@users.noreply.github.com>
2025-07-01 21:26:22 +0200

    fix: separate nginx server config

--- services/nginx/docker-entrypoint.sh
+++ services/nginx/docker-entrypoint.sh
@@ -5,10 +5,10 @@ CERT_PATH="/etc/letsencrypt/live/${REMOTE_DOMAIN}/fullchain.pem"
 CONF_DIR="/etc/nginx/conf.d"
 mkdir -p "$CONF_DIR"
 if [ -f "$CERT_PATH" ]; then
-    envsubst '$REMOTE_DOMAIN' < /etc/nginx/default.conf.template > "$CONF_DIR/default.conf"
+    envsubst '$REMOTE_DOMAIN' < /etc/nginx/conf.d/default.conf.template > "$CONF_DIR/default.conf"
 else
     echo "Using temporary HTTP config until certificate exists"
-    envsubst '$REMOTE_DOMAIN' < /etc/nginx/default.http.conf.template > "$CONF_DIR/default.conf"
+    envsubst '$REMOTE_DOMAIN' < /etc/nginx/conf.d/default.http.conf.template > "$CONF_DIR/default.conf"
 fi
 nginx -t
 exec nginx -g 'daemon off;'
